50c086f50d9e9772095e48faa89d6d4e7f7cf330
adib <adibraed1@gmail.com>
2022-10-13 19:15:10 -0300

    product card staging

--- client/src/components/ProductCard.tsx
+++ client/src/components/ProductCard.tsx
@@ -1,9 +1,24 @@
 import React from "react";
 // imagen
-//nombre
+// nombre
 // precio
-//rating
+// rating
+type Props = {
+  nombre: string;
+  precio: number;
+  rating: any;
+  imagen?: string;
+};
+const ProductCard = ({ nombre, precio, rating, imagen }: Props) => {
+  return (
+    <div>
+      <img src={imagen} alt="product" />
+      <h3>{nombre}</h3>
+      <h2>{precio}</h2>
+      <div>{rating}</div>
+      <button>Ver más</button>
+    </div>
+  );
+};
 
-export default function ProductCard() {
-  return <div>ProductCard</div>;
-}
+export default ProductCard;
