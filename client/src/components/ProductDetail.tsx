2409aad010dc11d92b3e35ba972e1a25e2fd337e
Riguja89 <jaimegutierrios@gmail.com>
2022-10-13 19:32:48 -0500

    Product Detail feature

--- client/src/components/ProductDetail.tsx
+++ client/src/components/ProductDetail.tsx
@@ -1,9 +1,30 @@
-import React from "react";
-// imagen
-//nombre
-// precio
-//rating
+import { produceWithPatches } from "immer";
+import React,{useEffect} from "react";
 
-export default function ProductDetail() {
-  return <div>ProductDetail</div>;
+
+type Props = {
+    nombre: string;
+    precio: number;
+    rating: any;
+    imagen?: string;
+    description: string
+  };
+
+export default function ProductDetail({nombre,precio,rating,imagen,description}:Props) {
+    useEffect(()=>{
+        
+    });
+  return <div>
+    <img src={imagen} alt="" />
+    <h1>{nombre}</h1>
+    <h2>${precio}</h2>
+    <form action="">
+    <label htmlFor="">Cantidad: </label>
+    <input type="cantidad" />
+    <button>Agregar al Carrito</button>
+    </form>
+    <label htmlFor="">Rating: {rating}</label>
+    <label htmlFor="">Descripción</label>
+    <p>{description}</p>
+    </div>;
 }
