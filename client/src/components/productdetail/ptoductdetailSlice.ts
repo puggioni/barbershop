5f42d9be62cd941863947f1929b3a642dc9ae79d
Riguja89 <jaimegutierrios@gmail.com>
2022-10-14 18:45:05 -0500

    Detalles al product detail

--- client/src/components/productdetail/ptoductdetailSlice.ts
+++ client/src/components/productdetail/ptoductdetailSlice.ts
@@ -23,15 +23,16 @@ export const productdetailSlice = createSlice({
   initialState,
   reducers: {
     getProductDetail:(state, action:PayloadAction<string>)=>{
-        console.log('Acá se haría el fetch al back para conseguir el detalle del producto; '+action.payload)
-        
-        if(typeof(data.find(element=>element.name===action.payload))==="object"){
-          console.log(typeof( data.find(element=>element.name===action.payload)))
+        //console.log('Acá se haría el fetch al back para conseguir el detalle del producto; '+action.payload)
           const aux = data.find(element=>element.name===action.payload)
           if(typeof aux==="object")Object.assign(state,aux)
-          console.log(aux);
-        }
+       
+        
     },
+    clearProducDetail:(state)=>{
+      Object.assign(state,initialState)
+     
+    }
     // increment: (state) => {
     //   // Redux Toolkit allows us to write "mutating" logic in reducers. It
     //   // doesn't actually mutate the state because it uses the Immer library,
@@ -49,6 +50,6 @@ export const productdetailSlice = createSlice({
 })
 
 // Action creators are generated for each case reducer function
-export const { getProductDetail} = productdetailSlice.actions
+export const { getProductDetail, clearProducDetail} = productdetailSlice.actions
 
 export default productdetailSlice.reducer
