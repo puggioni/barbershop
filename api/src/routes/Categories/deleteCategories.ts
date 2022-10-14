06eab6d90967ed76976ba85ab052dceb008dbedc
gon2santos <necchigonzalo@gmail.com>
2022-10-14 16:35:09 -0300

    fix(routes/categories) changed db delete attribute

--- api/src/routes/Categories/deleteCategories.ts
+++ api/src/routes/Categories/deleteCategories.ts
@@ -7,7 +7,7 @@ const router = Router();
 router.delete("/delete", async (req, res) => {
   const { name } = req.body;
   try {
-    const categoryDeleted = await CategoryModel.findOneAndDelete(name);
+    const categoryDeleted = await CategoryModel.findOneAndDelete({name : name});
     res.send(categoryDeleted);
   } catch (error) {
     res.status(500).send({error});
