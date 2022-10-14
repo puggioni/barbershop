b7751e9a495185648a6bc133a634b4da22f481cd
adib <adibraed1@gmail.com>
2022-10-14 20:11:06 -0300

    Agrega home component

--- client/src/components/productdetail/ProductDetail.tsx
+++ client/src/components/productdetail/ProductDetail.tsx
@@ -1,34 +1,37 @@
-import { produceWithPatches } from "immer";
-import React,{useEffect} from "react";
-import {useSelector,useDispatch} from "react-redux"
+import React, { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
 import { RootState } from "../../app/store";
 import { getProductDetail } from "./ptoductdetailSlice";
-import {useParams} from "react-router";
+import { useParams } from "react-router";
 
 type QuizParams = {
-  id: string
+  id: string;
 };
 
 export default function ProductDetail() {
-  const dispatch =useDispatch();
-  const {id}=useParams<QuizParams>();
-  const {name,precio,rating,imagen,description} =useSelector((state: RootState)=>state.producdetail);
-    useEffect(()=>{
-      if(typeof id==="string"){
-        dispatch(getProductDetail(id))
-      }
-    },[id]);
-  return <div className="bg-slate-200/50">
-    <img src={imagen} alt="" />
-    <h1>{name}</h1>
-    <h2>${precio}</h2>
-    <form action="">
-    <label htmlFor="">Cantidad: </label>
-    <input type="cantidad" />
-    <button>Agregar al Carrito</button>
-    </form>
-    <label htmlFor="">Rating: {rating}</label>
-    <label htmlFor="">Descripción</label>
-    <p>{description}</p>
-    </div>;
+  const dispatch = useDispatch();
+  const { id } = useParams<QuizParams>();
+  const { name, precio, rating, imagen, description } = useSelector(
+    (state: RootState) => state.producdetail
+  );
+  useEffect(() => {
+    if (typeof id === "string") {
+      dispatch(getProductDetail(id));
+    }
+  }, [dispatch, id]);
+  return (
+    <div className="bg-slate-200/50">
+      <img src={imagen} alt="" />
+      <h1>{name}</h1>
+      <h2>${precio}</h2>
+      <form action="">
+        <label htmlFor="">Cantidad: </label>
+        <input type="cantidad" />
+        <button>Agregar al Carrito</button>
+      </form>
+      <label htmlFor="">Rating: {rating}</label>
+      <label htmlFor="">Descripción</label>
+      <p>{description}</p>
+    </div>
+  );
 }
--- client/src/components/productdetail/ptoductdetailSlice.ts
+++ client/src/components/productdetail/ptoductdetailSlice.ts
@@ -1,54 +1,39 @@
-import { createSlice } from '@reduxjs/toolkit'
-import type { PayloadAction } from '@reduxjs/toolkit'
-import data from "../../mockdata"
+import { createSlice } from "@reduxjs/toolkit";
+import type { PayloadAction } from "@reduxjs/toolkit";
+import data from "../../mockdata";
 
 export interface ProductdetailState {
-    name: string;
-    precio: number;
-    rating: number;
-    imagen?: string;
-    description: string
+  name: string;
+  precio: number;
+  rating: number;
+  imagen?: string;
+  description: string;
 }
 
 const initialState: ProductdetailState = {
-    name: "",
-    precio: 0,
-    rating: 0,
-    imagen: "",
-    description: ""
-}
+  name: "",
+  precio: 0,
+  rating: 0,
+  imagen: "",
+  description: "",
+};
 
 export const productdetailSlice = createSlice({
-  name: 'productdetail',
+  name: "productdetail",
   initialState,
   reducers: {
-    getProductDetail:(state, action:PayloadAction<string>)=>{
-        console.log('Acá se haría el fetch al back para conseguir el detalle del producto; '+action.payload)
-        
-        if(typeof(data.find(element=>element.name===action.payload))==="object"){
-          console.log(typeof( data.find(element=>element.name===action.payload)))
-          const aux = data.find(element=>element.name===action.payload)
-          if(typeof aux==="object")Object.assign(state,aux)
-          console.log(aux);
-        }
+    getProductDetail: (state, action: PayloadAction<string>) => {
+      console.log(
+        "Acá se haría el fetch al back para conseguir el detalle del producto; " +
+          action.payload
+      );
+      const aux = data.find((element) => element.name === action.payload);
+      if (typeof aux === "object") Object.assign(state, aux);
     },
-    // increment: (state) => {
-    //   // Redux Toolkit allows us to write "mutating" logic in reducers. It
-    //   // doesn't actually mutate the state because it uses the Immer library,
-    //   // which detects changes to a "draft state" and produces a brand new
-    //   // immutable state based off those changes
-    //   state.value += 1
-    // },
-    // decrement: (state) => {
-    //   state.value -= 1
-    // },
-    // incrementByAmount: (state, action: PayloadAction<number>) => {
-    //   state.value += action.payload
-    // },
   },
-})
+});
 
 // Action creators are generated for each case reducer function
-export const { getProductDetail} = productdetailSlice.actions
+export const { getProductDetail } = productdetailSlice.actions;
 
-export default productdetailSlice.reducer
+export default productdetailSlice.reducer;
