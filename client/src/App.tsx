e058b66b0c05656421b46d02cd82e112c7ab34ec
ignaciansilva <ignacioansilva@gmail.com>
2022-10-15 11:29:21 -0300

    navBar & Searchbar(faltan detalles)

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import data from "./mockdata";
 import ProductCard from "./components/ProductCard";
+import NavBar from "./components/NavBar";
 function App() {
   return (
     <div>
+      <NavBar />
       {data.map((data) => (
         <ProductCard
           nombre={data.name}
