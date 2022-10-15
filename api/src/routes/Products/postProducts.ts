52bc1f7507f52df07b3e3cbb14946ab1492a4460
ManuelSeisdedos <seisdedosmanuel2@gmail.com>
2022-10-15 20:31:19 -0300

    Post Product Modified

--- api/src/routes/Products/postProducts.ts
+++ api/src/routes/Products/postProducts.ts
@@ -1,24 +1,34 @@
 import { Router } from "express";
-
-import ProductModel from "../../models/products";
+import Category from "../../models/categories";
+import Product from "../../models/products";
 
 const router = Router();
 
 router.post("/create", async (req, res) => {
   let { name, description, price, stock, available, favorite, categories } =
     req.body;
-  if(typeof(name) === 'string')
-    name = name.toLocaleLowerCase();
+  if (typeof name === "string") name = name.toLocaleLowerCase();
   try {
-    const response = await ProductModel.create({
+    const product = new Product({
       name: name,
       description: description,
       price: price,
       stock: stock,
       available: available,
+      favorite: favorite,
       categories: categories,
     });
-    res.status(200).send(response);
+
+    if (categories) {
+      const foundCategory = await Category.find({
+        name: { $in: categories },
+      });
+      product.categories = foundCategory.map((el) => el._id);
+    }
+    product.populate("categories", "name -_id");
+
+    const savedProduct = await product.save();
+    res.status(200).send(product);
   } catch (err) {
     res.status(500).send(err);
   }
