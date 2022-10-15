da8d1131b20b03b53bd27330efaaa4d8877c5569
Riguja89 <jaimegutierrios@gmail.com>
2022-10-14 22:50:31 -0500

    Produc detail agregó estilos

--- client/src/components/productdetail/ProductDetail.tsx
+++ client/src/components/productdetail/ProductDetail.tsx
@@ -14,6 +14,9 @@ export default function ProductDetail() {
   const {id}=useParams<QuizParams>();
   const {name,precio,rating,imagen,description} =useSelector((state: RootState)=>state.producdetail);
   let navigate = useNavigate();
+  const buttonStyle =
+  "m-auto px-3 py-1.5 bg-white rounded-lg border-2 border-black text-black";
+
     useEffect(()=>{
       if(typeof id==="string"){
         dispatch(getProductDetail(id))
@@ -29,19 +32,24 @@ export default function ProductDetail() {
         navigate(-1);
     }
 
-  return <div className="bg-slate-200/50">
-    <h1 onClick={goBack}>⬅ Go back</h1>
-  
-    <img src={imagen} alt="" />
-    <h1>{name}</h1>
-    <h2>${precio}</h2>
-    <form action="">
+  return <div className="bg-slate-100">
+    <h1 className=" mr-auto" onClick={goBack}>⬅ Go back</h1>
+    <div className="flex justify-center">
+    <img className=" w-5/6  rounded-xl" src={imagen} alt="" />
+    </div>
+    <h1 className=" text-center font-bold p-5">{name}</h1>
+    <div className="flex justify-end ">
+    <h2 className=" font-bold justify-end px-5 text-lg" >${precio}</h2>
+    </div>
+    <form action="" className="ml-10 mb-5 justify-center">
     <label htmlFor="">Cantidad: </label>
-    <input type="cantidad" />
-    <button >Agregar al Carrito</button>
-    </form>
-    <label htmlFor="">Rating: {rating}</label>
-    <label htmlFor="">Descripción</label>
-    <p>{description}</p>
+    <input type="number" className="flex justify-center rounded-lg border-2 border-black text-black mb-5"/>
+    <button className={buttonStyle}>Agregar al Carrito</button>
+    </form>    
+    <div>
+    <label htmlFor="" className=" font-bold ml-3">Descripción: </label>
+    <p className="ml-3">{description}</p>
+    </div>
+    <label htmlFor="" className=" p-5">Rating: {rating}  ✩</label>
     </div>;
 }
