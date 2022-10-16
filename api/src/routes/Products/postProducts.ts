6f25fca6d6ab83b0b6c656148d25f9eb129ea5b7
Agustin Puggioni <agusinpuggioni98@gmail.com>
2022-10-16 17:47:03 -0300

    fixes

--- api/src/routes/Products/postProducts.ts
+++ api/src/routes/Products/postProducts.ts
@@ -1,23 +1,35 @@
 import { Router } from "express";
 
-import ProductModel from "../../models/products";
-import { verifyToken, isAdmin } from "../Auth/middlewares";
+import Category from "../../models/categories";
+import Product from "../../models/products";
 
 const router = Router();
 
-router.post("/create", verifyToken, isAdmin, async (req, res) => {
-  const { name, description, price, stock, available, favorite, categories } =
+router.post("/create", async (req, res) => {
+  let { name, description, price, stock, available, favorite, categories } =
     req.body;
+  if (typeof name === "string") name = name.toLocaleLowerCase();
   try {
-    const response = await ProductModel.create({
+    const product = new Product({
       name: name,
       description: description,
       price: price,
       stock: stock,
       available: available,
+      favorite: favorite,
       categories: categories,
     });
-    res.status(200).send(response);
+
+    if (categories) {
+      const foundCategory = await Category.find({
+        name: { $in: categories },
+      });
+      product.categories = foundCategory.map((el) => el._id);
+    }
+    product.populate("categories", "name -_id");
+
+    const savedProduct = await product.save();
+    res.status(200).send(product);
   } catch (err) {
     console.log(err);
     res.status(500).send(err);
