fd918184a71950011a04e5e0c1c574358f592de4
ManuelSeisdedos <seisdedosmanuel2@gmail.com>
2022-10-21 00:50:27 -0300

    Back de pasarela de pagos - Faltan detalles

--- api/dist/routes/Reviews/editReview.js
+++ api/dist/routes/Reviews/editReview.js
@@ -13,27 +13,27 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = require("express");
-const productReviews_1 = __importDefault(require("../../models/productReviews"));
+const ProductReviews_1 = __importDefault(require("../../models/ProductReviews"));
 const products_1 = __importDefault(require("../../models/products"));
-const middlewares_1 = require("../Auth/middlewares");
+const auth_1 = require("../../middlewares/auth");
 const router = (0, express_1.Router)();
-router.patch("/edit/:IdReview", middlewares_1.verifyToken, (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+router.patch("/edit/:IdReview", auth_1.verifyToken, (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     let { rating, comment } = req.body;
     const { IdReview } = req.params;
     let oldRating;
     try {
-        yield productReviews_1.default.findById(IdReview)
+        yield ProductReviews_1.default.findById(IdReview)
             .then((review) => {
             oldRating = review.rating;
             rating ? (review.rating = rating) : {};
             comment ? (review.comment = comment) : {};
             return review.save();
         })
-            .then(savedReview => {
+            .then((savedReview) => {
             res.status(200).send(savedReview);
             return products_1.default.findOne({ reviews: IdReview });
         })
-            .then(product => {
+            .then((product) => {
             product.rating_sum -= oldRating;
             product.rating_sum += rating;
             product.rating = product.rating_sum / product.reviews.length;
---
+++ api/src/routes/Payments/Paypal.ts
@@ -0,0 +1,68 @@
+import axios from "axios";
+import { Router } from "express";
+
+const router = Router();
+
+router.post("/create-order", async (req, res) => {
+  try {
+    const order = {
+      intent: "CAPTURE",
+      purchase_units: [
+        {
+          amount: { currency_code: "USD", value: "333" },
+          description: "VENTA",
+        },
+      ],
+      application_context: {
+        brand_name: "Henry BarberShop",
+        landing_page: "LOGIN",
+        user_action: "PAY_NOW",
+        return_url: "http://localhost:5000/payments/capture-order",
+        cancel_url: "http://localhost:5000/payments/cancel-order",
+      },
+    };
+
+    const response = await axios.post(
+      `https://api-m.sandbox.paypal.com/v2/checkout/orders`,
+      order,
+      {
+        auth: {
+          username:
+            "AVwlVSANTKRUrYDVQ0bmVEjUqaC9-RHw8qn3uRVp-xr4SzQae-1GmM4-B-V4y_bP2tCw7gKH2S8SfeKx",
+          password:
+            "EG_ZGG1BcPvJhGKbU0HafZRgg1mFMRGk0kZVULdRAL-ECDr5IYVzvA1aWNPXiWQHcSRHqxooNZnyoy6Z",
+        },
+      }
+    );
+    //                  ACA DEBERIA CAMBIAR EL ESTADO DE LA ORDEN DE CAPTURANDO - PAGADA - ENVIADA
+    res.status(200).json(response.data);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+router.get("/capture-order", async (req, res) => {
+  const { token, PayerID } = req.query;
+
+  const response = await axios.post(
+    `https://api-m.sandbox.paypal.com/v2/checkout/orders/${token}/capture`,
+    {},
+    {
+      auth: {
+        username:
+          "AVwlVSANTKRUrYDVQ0bmVEjUqaC9-RHw8qn3uRVp-xr4SzQae-1GmM4-B-V4y_bP2tCw7gKH2S8SfeKx",
+        password:
+          "EG_ZGG1BcPvJhGKbU0HafZRgg1mFMRGk0kZVULdRAL-ECDr5IYVzvA1aWNPXiWQHcSRHqxooNZnyoy6Z",
+      },
+    }
+  );
+
+  console.log(response.data);
+  res.status(200).send("capture");
+});
+
+router.get("/cancel-order", (req, res) => {
+  res.redirect("http://localhost:5000/products");
+});
+
+export default router;
