3fd2857400bca56ab8810ba37d9c872baa9a7a35
gon2santos <necchigonzalo@gmail.com>
2022-10-22 09:59:43 -0300

    fix(user/signUp): add image attribute

--- api/src/routes/User/signup.ts
+++ api/src/routes/User/signup.ts
@@ -5,14 +5,7 @@ import jwt from "jsonwebtoken";
 const router = Router();
 
 router.post("/signup", async (req, res) => {
-  const {
-    name: name,
-    lastname: lastname,
-    email: email,
-    password: password,
-    phone_number: phone_number,
-    role: role,
-  } = req.body;
+  const { name, lastname, email, password, phone_number, image, role } = req.body;
 
   try {
     const user = new User({
@@ -21,6 +14,7 @@ router.post("/signup", async (req, res) => {
       email: email,
       password: await User.encryptPassword(password),
       phone_number: phone_number,
+      user_image: image,
       role: role,
     });
 
