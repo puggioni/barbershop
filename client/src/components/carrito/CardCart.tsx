47cba9eff74679c981e209f3f71607d36aea09dc
adib <adibraed1@gmail.com>
2022-10-24 18:31:53 -0300

    estilo carrito

--- client/src/components/carrito/CardCart.tsx
+++ client/src/components/carrito/CardCart.tsx
@@ -1,36 +1,68 @@
+import { useState } from "react";
 import { FaTrash } from "react-icons/fa";
+import { HiOutlineArrowLongDown, HiOutlineArrowLongUp } from "react-icons/hi2";
 import { Link } from "react-router-dom";
 
 const ProductCard = (producto: any) => {
-  const handleClick = (
+  const [cantidad, setCantidad] = useState(producto.cantidad);
+  const prodLocalStorage: any = JSON.parse(
+    window.localStorage.getItem("product") || "[]"
+  );
+  const handleDelete = (
     event: React.MouseEvent<SVGElement, MouseEvent>,
     id: string
   ) => {
     event.preventDefault();
 
-    let productos: any = JSON.parse(
-      window.localStorage.getItem("product") || "[]"
-    );
-
-    const prod = productos.filter((p: any) => {
+    const prod = prodLocalStorage.filter((p: any) => {
       return p.productos._id !== id;
     });
     window.localStorage.setItem("product", JSON.stringify(prod));
-    window.location.reload();
+    producto.forceUpdate();
   };
 
+  const handleCantidadChange = (event: any, num: number) => {
+    event?.preventDefault();
+    setCantidad((prev: number) => prev + num);
+    console.log(cantidad);
+
+    const index = prodLocalStorage.findIndex((p: any) => {
+      return p.productos._id === producto._id;
+    });
+    prodLocalStorage[index] = { productos: producto, cantidad: cantidad };
+
+    window.localStorage.setItem("product", JSON.stringify(prodLocalStorage));
+
+    producto.forceUpdate();
+  };
   if (producto) {
     return (
       <div className="grid grid-cols-[.5fr_1fr_.2fr_.2fr_.2fr] mx-8 items-center">
         <img className="h-32 " src={producto.image} alt="product" />
 
         <Link to={`/product/${producto._id}`}>{producto.name}</Link>
-        <h2 className="">${producto.price}</h2>
-        <div className="">{producto.cantidad}</div>
+        <h2>${producto.price}</h2>
+        <div className="relative border border-black py-1 pr-2 mr-4 items-center text-center">
+          <div>{cantidad}</div>
+          <HiOutlineArrowLongDown
+            onClick={(e) => {
+              handleCantidadChange(e, -1);
+            }}
+            size={10}
+            className="absolute bottom-0 right-0 cursor-pointer mb-1"
+          />
+          <HiOutlineArrowLongUp
+            onClick={(e) => {
+              handleCantidadChange(e, 1);
+            }}
+            size={10}
+            className="absolute top-0 right-0 cursor-pointer mt-1"
+          />
+        </div>
 
         <FaTrash
           onClick={(e) => {
-            handleClick(e, producto._id);
+            handleDelete(e, producto._id);
           }}
           size={25}
         />
