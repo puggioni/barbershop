77eb6efd09007285a1a1293451c444e2dc1f318c
Riguja89 <jaimegutierrios@gmail.com>
2022-10-25 10:58:25 -0500

    Se agrega componente turnos

--- client/src/components/Reserve/Reserve.tsx
+++ client/src/components/Reserve/Reserve.tsx
@@ -1,54 +1,57 @@
 import { VscArrowLeft } from "react-icons/vsc";
 import { useNavigate, Link } from "react-router-dom";
 import MapView from "./Map/MapView";
+import BarberCard from "./BarberCard";
+import { fetchAllBarbers } from "../slices/barbers";
+import { useCallback, useEffect } from "react";
+import { useAppDispatch, useAppSelector } from "../../app/hooks";
+import { RootState } from "../../app/store";
 
 const Reserve = () => {
-  let navigate = useNavigate();
+  const dispatch=useAppDispatch();
+  const inicializar = useCallback(async () => {
+    dispatch(fetchAllBarbers());}, [dispatch]);
+  useEffect(() => {
+    inicializar();
+  }, [inicializar]);
 
-  function goBack(): void {
-    navigate(-1);
-  }
+  const data = useAppSelector((state: RootState) => state.barbers);
 
-  const cardBarber =
-    "h-52 w-36 text-center rounded-lg font-bold  text-2xl text-black bg-slate-200/50	inline-block m-10";
-
-  return (
-    <div>
-      <VscArrowLeft
-        onClick={() => goBack()}
-        className="ml-12 my-3 h-6 w-6 fill-white"
-      />
-
-      <h2 className="text-center font-bold p-2 text-2xl text-white">
-        Seleccione una sucursal para reservar su turno:
-      </h2>
-
-      <div className="h-60 w-60 text-center font-bold flex justify-center min text-2xl text-white  bg-gray-800	m-auto">
-        Mapa
-      </div>
-
-      <div className=" flex justify-center min m-auto">
-        <MapView />
-      </div>
-
-      <div className=" justify-center min block m-auto">
-        <h2 className="text-center font-bold p-2 text-2xl text-white mt-8">
-          Barberos mas valorados:
-        </h2>
-
-        <div className="flex justify-center min">
-          <Link className={cardBarber} to="/reserve/barber">
-            <div>Barbero 4.8</div>
-          </Link>
-          <Link className={cardBarber} to="/reserve/barber">
-            <div>Barbero 4.8</div>
-          </Link>
-          <Link className={cardBarber} to="/reserve/barber">
-            <div>Barbero 5.0</div>
-          </Link>
-        </div>
+return(
+  <div className="flex text-white">
+    PEDÍ TU TURNO
+    <div >
+      <div className=" flex-col">
+        Seleccion del servicio
+        <button> Corte</button> <br />
+        <button>Afeitado</button> <br />
+        <button>Corte y Afeitado</button> <br />
       </div>
     </div>
-  );
+    <div className="text-black">
+    {data.allBarbers?.map((datas: any) => (
+  <BarberCard
+    name={datas.name}
+    image={datas.image}
+    rating={datas.rating}
+    office={datas.office}
+  />
+
+))}
+    </div>
+    <div>
+      <div>
+      Selecciona tu horario
+      <input  className="text-black" type="date" />
+      <select name="" id=""></select>
+      <select name="" id=""></select>
+      </div>
+    </div>
+  </div>
+)
+
 };
 export default Reserve;
+
+
+
