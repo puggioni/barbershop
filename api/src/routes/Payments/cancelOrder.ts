a17400545a9ed594f0e01bafd1836fe66c56717b
ManuelSeisdedos <seisdedosmanuel2@gmail.com>
2022-10-25 19:34:33 -0300

    Added: orders routes

--- api/src/routes/Payments/cancelOrder.ts
+++ api/src/routes/Payments/cancelOrder.ts
@@ -1,7 +1,19 @@
 import { Router } from "express";
+import Orders from "../../models/purchaseOrder";
 
 const router = Router();
-router.get("/cancel-order", (req, res) => {
-  res.redirect("htpp://localhost:5000/product");
+
+router.get("/cancel-order", async (req, res) => {
+  const { idOrder } = req.query;
+  try {
+    const order = await Orders.findById(idOrder);
+    order["state"] = "Cancelada";
+    order.save();
+    res.send(order).redirect("htpp://localhost:5000/product");
+  } catch (error) {
+    console.log(error);
+    res.status(500).send(error);
+  }
 });
+
 export default router;
