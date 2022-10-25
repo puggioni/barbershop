7d59ab58542ab50859bb600687dc42cfca96da54
Riguja89 <jaimegutierrios@gmail.com>
2022-10-25 16:03:58 -0500

    Se agrega componente turnos

--- client/src/components/Reserve/Reserve.tsx
+++ client/src/components/Reserve/Reserve.tsx
@@ -3,25 +3,29 @@ import { useNavigate, Link } from "react-router-dom";
 import MapView from "./Map/MapView";
 import BarberCard from "./BarberCard";
 import { fetchAllBarbers } from "../slices/barbers";
-import { useCallback, useEffect } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { useAppDispatch, useAppSelector } from "../../app/hooks";
 import { RootState } from "../../app/store";
+import Calendar from 'react-calendar';
 
 const Reserve = () => {
   const dispatch=useAppDispatch();
   const inicializar = useCallback(async () => {
     dispatch(fetchAllBarbers());}, [dispatch]);
+
   useEffect(() => {
     inicializar();
   }, [inicializar]);
 
+  const [value, onChange] = useState(new Date());
   const data = useAppSelector((state: RootState) => state.barbers);
-
+  const [turno, setTurno]=useState({});
+  
 return(
   <div className="flex text-white">
     PEDÍ TU TURNO
     <div >
-      <div className=" flex-col">
+      <div className=" flex-col"> <br />
         Seleccion del servicio
         <button> Corte</button> <br />
         <button>Afeitado</button> <br />
@@ -42,9 +46,14 @@ return(
     <div>
       <div>
       Selecciona tu horario
-      <input  className="text-black" type="date" />
-      <select name="" id=""></select>
-      <select name="" id=""></select>
+      <div className=" w-80 bg-white text-black text-center">
+      <Calendar onChange={onChange} value={value} />
+      </div>
+      <select name="" id="">Selecciona Turno
+      {}
+      </select>
+      <select name="" id="">Selecciona Sucursa</select>
+      <button>SOLICITAR TURNO</button>
       </div>
     </div>
   </div>
