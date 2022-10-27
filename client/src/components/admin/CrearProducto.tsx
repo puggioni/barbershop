f4466fe5b50a4b463a5495399214cceef3efb638
adib <adibraed1@gmail.com>
2022-10-27 15:19:43 -0300

    update

--- client/src/components/admin/CrearProducto.tsx
+++ client/src/components/admin/CrearProducto.tsx
@@ -50,53 +50,75 @@ const CrearProducto = () => {
         categorias: [...inputs.categorias, e.target.value],
       });
   };
+
+  const handleCreateOrder = () => {};
+  //===================render========================
   return (
     <div className="background flex bg-bg-prods bg-cover">
-      <div className="container h-screen !w-full mx-40 my-16 py-4 px-8 bg-white/50 rounded-lg">
+      <div className="flex flex-col container h-screen !w-full mx-40 my-16 py-4 px-8 bg-white/50 rounded-lg">
         <h1 className="text-5xl font-bold flex justify-center">
           CREAR PRODUCTO
         </h1>
         <div className="flex m-auto w-[40%] pt-4 border-b-2 border-black"></div>
-        <div className="container flex flex-row justify-around h-12 my-8">
+        <div className="container flex flex-row justify-around h-12 my-16">
           <input
             value={inputs.nombre}
+            type="text"
             name="nombre"
             onChange={(e) => handleInput(e)}
             className="rounded-lg bg-white/70 pl-4"
             placeholder="Nombre del producto"
           />
-          <input
-            value={inputs.precio}
-            name="precio"
-            onChange={(e) => handleInput(e)}
-            className="rounded-lg bg-white/70 pl-4"
-            placeholder="Precio"
-          />
-          <input
-            value={inputs.stock}
-            name="stock"
-            onChange={(e) => handleInput(e)}
-            className="rounded-lg bg-white/70 pl-4"
-            placeholder="Stock inicial"
-          />
+          <div>
+            <label htmlFor="precio">Precio: </label>
+            <input
+              value={inputs.precio}
+              type="number"
+              id="precio"
+              name="precio"
+              onChange={(e) => handleInput(e)}
+              className="rounded-lg bg-white/70 pl-4"
+              placeholder="Precio"
+            />
+          </div>
+          <div>
+            <label htmlFor="precio">Precio: </label>
+            <input
+              value={inputs.stock}
+              type="number"
+              name="stock"
+              onChange={(e) => handleInput(e)}
+              className="rounded-lg bg-white/70 pl-4"
+              placeholder="Stock inicial"
+            />
+          </div>
         </div>
 
         <textarea
           value={inputs.descripcion}
           name="descripcion"
           onChange={(e) => handleInput(e)}
-          className="flex m-auto w-[80%] rounded-lg bg-white/70 p-4 h-[50%] mb-8"
+          className="flex m-auto w-[80%] rounded-lg bg-white/70 p-4 h-[70%] mb-8"
           placeholder="Descripción"
         />
 
-        <input
-          type="file"
-          id="imagen"
-          accept="image/*"
-          className="chooseFile"
-        />
+        <div className="w-[10%] my-8 ml-[10%]">
+          <input
+            type="file"
+            id="imagen"
+            accept="image/*"
+            className="hidden left-[10%] bottom-1 my-2"
+          />
 
-        <div className="grid grid-cols-[.5fr_4fr] gap-3 max-w-[80%] m-auto">
+          <label
+            className="left-[10%] bottom-1 cursor-pointer my-2"
+            htmlFor="imagen"
+          >
+            <p className="bg-white/70 rounded-lg px-2">Choose file</p>
+          </label>
+        </div>
+
+        <div className="grid grid-cols-[.5fr_4fr] justify-self-center gap-4 w-[80%] m-auto">
           <select
             name="categorias"
             onChange={(e) => handleGenreChange(e)}
@@ -124,7 +146,16 @@ const CrearProducto = () => {
             })}
           </div>
         </div>
-        <button>CREAR PRODUCTO</button>
+        <div className="flex justify-end">
+          <button
+            onClick={() => {
+              handleCreateOrder();
+            }}
+            className="mt-20 mb-4 bg-black text-white w-[10%] rounded-sm p-2 "
+          >
+            CREAR PRODUCTO
+          </button>
+        </div>
       </div>
     </div>
   );
