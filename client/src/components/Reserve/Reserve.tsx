0648deb8c28fbe98d531b994993a1d4a364ad16b
Riguja89 <jaimegutierrios@gmail.com>
2022-10-27 15:55:47 -0500

    Feat Turnos funcionand

--- client/src/components/Reserve/Reserve.tsx
+++ client/src/components/Reserve/Reserve.tsx
@@ -8,16 +8,16 @@ import { useAppDispatch, useAppSelector } from "../../app/hooks";
 import { RootState } from "../../app/store";
 import { buttonHover } from "../NavBar";
 import { postAppointment } from "../slices/appoinment";
+import offices, { fetchAllOffices } from "../slices/offices";
 import Calendar from 'react-calendar';
 import 'react-calendar/dist/Calendar.css';
 
 const initialTurn={
   service:"",
   user: JSON.parse(window.localStorage.getItem("user")||'{"_id":""}')._id,
-  date:new Date(Date.now()).toLocaleDateString(),
-  horario:0,
+  date:new Date(Date.now()),
   barber:"",
-  sucursal:"",
+  office:"",
   block:0
 }
 const selected= "shadow-md hover:shadow-slate-500\tbg-[#855C20] text-white hover:ease-in-out duration-300"
@@ -28,7 +28,10 @@ const Reserve = () => {
 
   const inicializar = useCallback(async () => {
     setTurno({...turno,user: JSON.parse(window.localStorage.getItem("user")||'{"_id":""}')._id,});
-    dispatch(fetchAllBarbers());}, [dispatch,window.localStorage.getItem("user")]);
+    dispatch(fetchAllBarbers());
+    dispatch(fetchAllOffices());
+  }, [dispatch,window.localStorage.getItem("user")]);
+    
 
   useEffect(() => {
     inicializar();
@@ -36,6 +39,7 @@ const Reserve = () => {
 
   const [date, setDate] = useState(new Date(Date.now()));
   const data = useAppSelector((state: RootState) => state.barbers);
+  const sucursales = useAppSelector((state: RootState) => state.offices.allOffices);
 
 
   const serviceSelect=( e: any)=>{
@@ -46,25 +50,28 @@ const Reserve = () => {
     });
   }
   const  handleFormTurn=(e:any)=>{
-    e.preventDefault();
+    e.preventDefault()
+    if(e.target.name==="office") {
+      setTurno({...turno, barber:"",
+      [e.target.name]:e.target.value}) 
+    }else{
     setTurno({...turno,
-      [e.target.name]:e.target.value,
-      
-    });
+      [e.target.name]:e.target.value,})
+    };
   }
 
   const selectBarber=(e:any, barber:any)=>{
     e.preventDefault();
-    setTurno({...turno, barber: barber._id, sucursal:barber.office})
+    setTurno({...turno, barber: barber._id})
   }
 
   const sendTurno=(e: React.FormEvent<HTMLButtonElement>)=>{
     e.preventDefault();
-    turno.date=date.toLocaleDateString();
-    if(!turno.barber||!turno.horario||!turno.service||!turno.sucursal)
+   // turno.date=date.toLocaleDateString();
+    if(!turno.barber||!turno.block||!turno.service||!turno.office)
     {alert("Por favor asegurate de completar todos los campos")}
     else{
-      turno.block=turno.horario
+      turno.block=turno.block
       console.log(turno)
       dispatch(postAppointment(turno))
     }
@@ -105,10 +112,13 @@ return(
       </div><br />
       <h2 className="flex justify-center my-auto text-2xl text-black pb-10"> SELECCION DEL SUCURSAL </h2>
         
-      <select className="inline justify-center p-2  mt-1 py-1 rounded-lg  border border-black" name="officina" id="">SELECCIONE UNA SUCURSAL:
+      <select className="inline justify-center p-2  mt-1 py-1 rounded-lg  border border-black" name="office" id="">SELECCIONE UNA SUCURSAL:
       <option value="none">SELECCIONE UNA SUCURSAL:</option>
-      <option value="baires">Palermo, Buenos Aires</option>
-      <option value="cba">Cordoba, Capital</option>
+      { sucursales?.map(sucursal=>{ 
+                   return(
+                        <option key={sucursal._id} value={sucursal._id}>{sucursal.location?sucursal.location:"Sucursal X"}</option>
+                    )
+                })}
       </select>
       </div>
       
@@ -116,16 +126,17 @@ return(
     
     <div className="grid grid-cols-2  gap-4 m-10 mb-5">
     {data.allBarbers?.map((datas: any) => (
+      turno.office===datas.office?
       <div onClick={(e)=>selectBarber(e,datas)} 
-      className={`${turno.barber===datas._id?selected:""} p-0 m-0 rounded-lg`}>
-  <BarberCard
-    key={datas.name}
-    name={datas.name}
-    image={datas.image}
-    rating={datas.rating}
-    office={datas.office}
-  />
-      </div>
+      className={`${turno.barber===datas._id? selected:""} p-0 m-0 rounded-lg`}>
+      <BarberCard
+      key={datas.name}
+      name={datas.name}
+      image={datas.image}
+      rating={datas.rating}
+      office={datas.office}
+      /> 
+      </div>:<></>
 
 ))}
     </div>
@@ -141,7 +152,7 @@ return(
       </div>
       {/* <input  className="justify-center justify-center my-auto   text-xl  text-black" type="date" /> */}
       <br />
-      <select className="inline justify-center p-2  mt-5 py-1 rounded-lg border border-black" name="horario" id="">
+      <select className="inline justify-center p-2  mt-5 py-1 rounded-lg border border-black" name="block" id="">
       <option value="none">SELECCIONE UN HORARIO:</option>
       <option value="1">8:00hs</option>
       <option value="2">9:00hs</option>
