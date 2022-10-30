55760fbccde8a409b81dac4e13d98cb9515f6a55
ManuelSeisdedos <seisdedosmanuel2@gmail.com>
2022-10-30 14:05:51 -0300

    Fixed getPurchaseOrders

--- api/src/routes/Orders/getPurchaseOrders.ts
+++ api/src/routes/Orders/getPurchaseOrders.ts
@@ -3,13 +3,12 @@ import Orders from "../../models/purchaseOrder";
 
 const router = Router();
 
-router.get("/all", async (req, res) => {
+router.get("/all-orders", async (req, res) => {
   try {
-    const orders = await Orders.find();
-    res.send(orders);
-  } catch (err) {
-    console.log(err);
-    res.status(500).send(err);
+    const orders = await Orders.find({});
+    res.status(200).send(orders);
+  } catch (error) {
+    res.status(500).send(error);
   }
 });
 
