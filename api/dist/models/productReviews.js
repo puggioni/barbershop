b7bacb85923023579a9a820d8ad6e193c28b7c45
Adib Raed <adibraed1@gmail.com>
2022-10-30 11:09:50 -0300

    Update productReviews.js

--- api/dist/models/productReviews.js
+++ api/dist/models/productReviews.js
@@ -5,13 +5,22 @@ const reviewsSchema = new mongoose_1.Schema({
     rating: {
         required: true,
         type: Number,
+        min: 0,
+        max: 5
     },
     comment: {
         type: String,
         required: true,
+        validate: {
+            validator: function (v) {
+                return ((v.length > 10) && (v.length < 100));
+            },
+            message: "Comments must be between 10 and 100 chars long!"
+        },
     }
 }, {
     versionKey: false,
     timestamps: true,
 });
 exports.default = (0, mongoose_1.model)("Reviews", reviewsSchema);
+
