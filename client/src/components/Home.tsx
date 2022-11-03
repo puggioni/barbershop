2eb3a6df8386b20122ba8a7c363a92d85ec76679
adib <adibraed1@gmail.com>
2022-11-03 16:14:23 -0300

    estilo responsive login signup passreset done

--- client/src/components/Home.tsx
+++ client/src/components/Home.tsx
@@ -1,14 +1,35 @@
 import { Link } from "react-router-dom";
+// import { useAppSelector } from "../app/hooks";
 import Footer from "./Footer";
+
 export const buttonStyle =
   "m-auto px-3 py-1.5 bg-white rounded-lg border-2 border-black text-black hover:bg-black hover:text-white";
+export const buttonHover =
+  "hover:shadow-md hover:shadow-slate-500 hover:bg-[#855C20] hover:text-white hover:ease-in-out hover:duration-300";
 
 const Home = () => {
+  // const logeado = useAppSelector((state) => state.logIn.logeado);
+
   return (
     <div
-      className=" lg:h-[75vh] lg:bg-transparent text-white 
+      className="lg:bg-transparent text-white lg:min-h-0
     lg:bg-home-bg bg-cover overflow-hidden bg-no-repeat bg-center bg-home-responsive min-h-screen "
     >
+      <div className="inline-block float-right m-6">
+        <Link
+          className={` bg-[#C0914C] lg:hidden text-white px-2 py-2 rounded-lg font-bold`}
+          to={"/user/login"}
+        >
+          Log In
+        </Link>
+        <Link
+          className={` ml-6 bg-[#C0914C] lg:hidden text-white px-2 py-2 rounded-lg font-bold`}
+          to={"/user/create"}
+        >
+          Sign in
+        </Link>
+      </div>
+
       <div className="h-[75vh]">
         <Link
           className="bg-[#855C20] py-4 px-4 font-bold text-3xl lg:block absolute hidden left-16 bottom-48"
@@ -17,7 +38,7 @@ const Home = () => {
           Pedi tu turno
         </Link>
         <Link
-          className="bg-[#855C20] lg:hidden lg:rounded-none rounded-xl block py-4 px-4 font-bold text-3xl absolute lg:left-16 lg:bottom-48 left-8 bottom-8 z-0"
+          className="bg-[#C0914C] lg:hidden rounded-xl inline-block py-4 px-4 font-bold text-3xl absolute lg:left-16 lg:bottom-48 left-8 bottom-8 z-0"
           to="/reserve"
         >
           Turnos
